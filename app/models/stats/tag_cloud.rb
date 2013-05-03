4d22c88ea5e497e61cfa6082c67c29742fae3dde
Ruy Diaz <diazruy@gmail.com>
2013-05-02 18:59:40 -0700

    Extract tags method

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -1,32 +1,38 @@
 # tag cloud code inspired by this article
 #  http://www.juixe.com/techknow/index.php/2006/07/15/acts-as-taggable-tag-cloud/
 class TagCloud
-  attr_accessor :user, :tags, :divisor, :min
+  attr_accessor :user, :divisor, :min
 
   def initialize(user, cut_off=nil)
     @user = user
     @cut_off = cut_off
   end
 
-  # TODO: parameterize limit
   def compute
-    levels=10
-
-    params = [sql(@cut_off), user.id]
-    if @cut_off
-      params += [@cut_off, @cut_off]
-    end
-    @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
+    @tags = tags
     max, @min = 0, 0
     @tags.each { |t|
       max = [t.count.to_i, max].max
       @min = [t.count.to_i, @min].min
     }
     @divisor = ((max - @min) / levels) + 1
+  end
 
+  def tags
+    return @tags if @tags
+    params = [sql(@cut_off), user.id]
+    if @cut_off
+      params += [@cut_off, @cut_off]
+    end
+    @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
   end
 
 private
+  def levels
+    10
+  end
+
+  # TODO: parameterize limit
   def sql(cut_off=nil)
     query = "SELECT tags.id, tags.name, count(*) AS count"
     query << " FROM taggings, tags, todos"
